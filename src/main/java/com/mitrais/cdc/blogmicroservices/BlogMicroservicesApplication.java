a041b0acb7b3699359e9d444e886ba39b05d2a75
Syarif Hidayat <srf.hidayat@gmail.com>
2019-08-20 14:07:42 +0700

    first commit

---
+++ src/main/java/com/mitrais/cdc/blogmicroservices/BlogMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mitrais.cdc.blogmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mitrais/cdc/blogmicroservices/controller/BlogController.java
@@ -0,0 +1,17 @@
+package com.mitrais.cdc.blogmicroservices.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class BlogController {
+
+
+    @RequestMapping("/test")
+    public ResponseEntity<?> test(){
+        return ResponseEntity.ok("Test");
+    }
+}
