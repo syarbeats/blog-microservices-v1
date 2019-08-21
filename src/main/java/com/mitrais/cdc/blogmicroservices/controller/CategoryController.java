9562265ce8b36b8bdf292554a1f47a104a6d8971
Syarif Hidayat <srf.hidayat@gmail.com>
2019-08-21 13:23:22 +0700

    Add Post, Comment, Category Controller.

---
+++ src/main/java/com/mitrais/cdc/blogmicroservices/controller/CategoryController.java
@@ -0,0 +1,74 @@
+package com.mitrais.cdc.blogmicroservices.controller;
+
+import com.mitrais.cdc.blogmicroservices.exception.BadRequestAlertException;
+import com.mitrais.cdc.blogmicroservices.payload.CategoryPayload;
+import com.mitrais.cdc.blogmicroservices.services.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+    private final Logger log = LoggerFactory.getLogger(CategoryController.class);
+
+    private static final String ENTITY_NAME = "blogCategory";
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<CategoryPayload> createCategory(@Valid @RequestBody CategoryPayload categoryDTO) throws URISyntaxException {
+        log.debug("REST request to save Category : {}", categoryDTO);
+        if (categoryDTO.getId() != null) {
+            throw new BadRequestAlertException("A new category cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        CategoryPayload result = categoryService.save(categoryDTO);
+        return ResponseEntity.created(new URI("/api/categories/" + result.getId())).body(result);
+    }
+
+    @PutMapping("/categories")
+    public ResponseEntity<CategoryPayload> updateCategory(@Valid @RequestBody CategoryPayload categoryDTO) throws URISyntaxException {
+        log.debug("REST request to update Category : {}", categoryDTO);
+        if (categoryDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        CategoryPayload result = categoryService.save(categoryDTO);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/categories")
+    public List<CategoryPayload> getAllCategories() {
+        log.debug("REST request to get all Categories");
+        return categoryService.findAll();
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryPayload> getCategory(@PathVariable Long id) {
+        log.debug("REST request to get Category : {}", id);
+        CategoryPayload categoryDTO = categoryService.findOne(id).get();
+        return ResponseEntity.ok(categoryDTO);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        log.debug("REST request to delete Category : {}", id);
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/mitrais/cdc/blogmicroservices/controller/CommentController.java
@@ -0,0 +1,75 @@
+package com.mitrais.cdc.blogmicroservices.controller;
+
+import com.mitrais.cdc.blogmicroservices.exception.BadRequestAlertException;
+import com.mitrais.cdc.blogmicroservices.payload.CommentPayload;
+import com.mitrais.cdc.blogmicroservices.services.CommentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class CommentController {
+
+    private final Logger log = LoggerFactory.getLogger(CommentController.class);
+
+    private static final String ENTITY_NAME = "blogComment";
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/comments")
+    public ResponseEntity<CommentPayload> createComment(@RequestBody CommentPayload commentDTO) throws URISyntaxException {
+        log.debug("REST request to save Comment : {}", commentDTO);
+        if (commentDTO.getId() != null) {
+            throw new BadRequestAlertException("A new comment cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        CommentPayload result = commentService.save(commentDTO);
+        return ResponseEntity.created(new URI("/api/comments/" + result.getId())).body(result);
+    }
+
+
+    @PutMapping("/comments")
+    public ResponseEntity<CommentPayload> updateComment(@RequestBody CommentPayload commentDTO) throws URISyntaxException {
+        log.debug("REST request to update Comment : {}", commentDTO);
+        if (commentDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        CommentPayload result = commentService.save(commentDTO);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/comments")
+    public List<CommentPayload> getAllComments() {
+        log.debug("REST request to get all Comments");
+        return commentService.findAll();
+    }
+
+    @GetMapping("/comments/{id}")
+    public ResponseEntity<CommentPayload> getComment(@PathVariable Long id) {
+        log.debug("REST request to get Comment : {}", id);
+        CommentPayload commentDTO = commentService.findOne(id).get();
+        return ResponseEntity.ok(commentDTO);
+    }
+
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+        log.debug("REST request to delete Comment : {}", id);
+        commentService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
