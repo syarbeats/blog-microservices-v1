75758cb6c3a8f6c9e30d9c7b369d881414a4cdbd
Syarif Hidayat <syarif.hidayat@mitrais.com>
2019-12-09 13:11:03 +0700

    Add Eureka Client config.

--- src/main/java/com/mitrais/cdc/blogmicroservices/BlogMicroservicesApplication.java
+++ src/main/java/com/mitrais/cdc/blogmicroservices/BlogMicroservicesApplication.java
@@ -2,8 +2,12 @@ package com.mitrais.cdc.blogmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@RefreshScope
+@EnableEurekaClient
 public class BlogMicroservicesApplication {
 
     public static void main(String[] args) {
